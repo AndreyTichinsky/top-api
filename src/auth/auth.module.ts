7180f3247d885b59effd607da612a6dc75a934be
Andrey Tychinskiy <andreytichinsky@gmail.com>
2022-07-29 11:26:11 +0300

    Add tests and analytics tools

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
-import { Collection } from 'mongoose';
+import { UserModel } from './user.model';
 
 @Module({
   controllers: [AuthController],
   imports: [
     TypegooseModule.forFeature([
-      { typegooseClass: AuthModule, schemaOptions: { collection: 'Auth' } },
+      { typegooseClass: UserModel, schemaOptions: { collection: 'Auth' } },
     ]),
   ],
 })
