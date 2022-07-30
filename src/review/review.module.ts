64070bc231ca07f0a78029074c8c85c0042006bd
Andrey Tychinskiy <andreytichinsky@gmail.com>
2022-07-30 16:46:13 +0300

    add telegram logic

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,10 +3,12 @@ import { TypegooseModule } from 'nestjs-typegoose';
 import { ReviewController } from './review.controller';
 import { ReviewModel } from './review.model';
 import { ReviewService } from './review.service';
+import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
   controllers: [ReviewController],
   imports: [
+    TelegramModule,
     TypegooseModule.forFeature([
       {
         typegooseClass: ReviewModel,
